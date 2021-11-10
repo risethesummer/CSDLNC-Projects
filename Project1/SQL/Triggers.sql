56a9730e673890a4c17941efc78fa0d87a3aa2fa
nquangphu <64113119+nquangphu@users.noreply.github.com>
2021-11-10 20:58:03 +0700

    Update triggers

--- Project1/SQL/Triggers.sql
+++ Project1/SQL/Triggers.sql
@@ -1,54 +1,23 @@
--- GO
--- CREATE TRIGGER TRG_HDCT_GiaBan_InsteadOfInsert
--- ON HoaDonChiTiet
--- INSTEAD OF INSERT
--- AS
--- BEGIN
--- 	INSERT INTO HoaDonChiTiet(MaHD, MaSP, SoLuong, GiaBan, GiaGiam, ThanhTien)
--- 	SELECT MaHD, MaSP, SoLuong, SP.Gia, GiaGiam, (SP.Gia - GiaGiam) * SoLuong
--- 	From inserted INNER JOIN SanPham SP
--- 		ON inserted.MaSP = SP.Ma
-	
--- 	UPDATE HoaDon
--- 	SET TongTien = (SELECT SUM(HDCT.ThanhTien)
--- 					FROM HoaDonChiTiet HDCT
--- 					WHERE Ma = HDCT.MaHD)
--- 	WHERE Ma = (SELECT TOP 1 i.MaHD
--- 				FROM inserted i)
--- END
--- GO
-
--- GO
--- CREATE TRIGGER TRG_HDCT_GiaBan_GiaGiam_AfterInsertUpdate
--- ON HoaDonChiTiet
--- AFTER INSERT, UPDATE
--- AS
--- BEGIN
--- 	IF EXISTS (SELECT * 
--- 				FROM inserted i
--- 				WHERE i.ThanhTien < 0)
--- 		BEGIN
--- 			ROLLBACK TRANSACTION
--- 		END
--- END
--- GO
-
 USE cua_hang;
-
-DROP TRIGGER TRG_HDCT_HoaDonChiTiet_InsteadOfInsert,TRG_HDCT_HoaDonChiTiet_InsteadOfUpdate,TRG_HDCT_HoaDonChiTiet_InsteadOfDelete;
-
-
 GO
+DROP TRIGGER TRG_HDCT_HoaDonChiTiet_InsteadOfInsert, TRG_HDCT_HoaDonChiTiet_InsteadOfUpdate, TRG_HDCT_HoaDonChiTiet_InsteadOfDelete
+GO
+
+--------------------------------------------------------
+-- a. Thanh tien CTHD= (So luong * (Gia ban-Gia giam)) #
+-- b. Tong tien (mahd) = sum(ThanhTien)cthd(mahd)      #
+--------------------------------------------------------
+
+------------------------- INSERT -------------------------
+----------------------------------------------------------
 CREATE TRIGGER TRG_HDCT_HoaDonChiTiet_InsteadOfInsert
 ON HoaDonChiTiet
 INSTEAD OF INSERT
 AS
 BEGIN
-	
 	INSERT INTO HoaDonChiTiet(MaHD, MaSP, SoLuong, GiaBan, GiaGiam, ThanhTien)
 	SELECT MaHD, MaSP, SoLuong, SP.Gia, GiaGiam, (SP.Gia - GiaGiam) * SoLuong
-	From inserted INNER JOIN SanPham SP
-	ON inserted.MaSP = SP.Ma
+	From inserted INNER JOIN SanPham SP ON inserted.MaSP = SP.Ma
 
 	UPDATE HoaDon
 		SET TongTien += SUM_INSERTED.SUM_THANHTIEN
@@ -57,17 +26,17 @@ BEGIN
 			FROM INSERTED INNER JOIN SanPham SP ON inserted.MaSP = SP.Ma
 			GROUP BY MaHD
 		) SUM_INSERTED
-	WHERE HoaDon.Ma = SUM_INSERTED.MaHD;
+		WHERE HoaDon.Ma = SUM_INSERTED.MaHD;
 END
-GO
 
+------------------------- UPDATE -------------------------
+----------------------------------------------------------
 GO
 CREATE TRIGGER TRG_HDCT_HoaDonChiTiet_InsteadOfUpdate
 ON HoaDonChiTiet
 INSTEAD OF UPDATE
 AS
 BEGIN
-
 	UPDATE HoaDonChiTiet -- MaHD, MaSP is key so we can not update
 	SET SoLuong = INSERTED.SoLuong, 
 	GiaBan = SP.Gia,
@@ -76,24 +45,23 @@ BEGIN
 	FROM INSERTED INNER JOIN SanPham SP ON SP.Ma = INSERTED.MaSP
 	WHERE HoaDonChiTiet.MaHD = INSERTED.MaHD AND HoaDonChiTiet.MaSP = INSERTED.MaSP
 
-
 	UPDATE HoaDon
-	SET TongTien = TongTien - SUM_UPDATED.SUM_THANHTIEN_TRU + SUM_UPDATED.SUM_THANHTIEN_CONG  
+	SET TongTien = TongTien - SUM_UPDATED.SUM_THANHTIEN_BOT + SUM_UPDATED.SUM_THANHTIEN_THEM  
 	FROM ( -- For an UPDATE, Inserted contains the new values (after the update) while Deleted contains the old values before the update. 
 		SELECT 
-		INSERTED.MaHD,
-		SUM((SP.Gia - INSERTED.GiaGiam) * INSERTED.SoLuong) SUM_THANHTIEN_CONG,
-		SUM((SP.Gia - DELETED.GiaGiam) * DELETED.SoLuong) SUM_THANHTIEN_TRU
+			INSERTED.MaHD,
+			SUM((SP.Gia - INSERTED.GiaGiam) * INSERTED.SoLuong) AS SUM_THANHTIEN_THEM,
+			SUM((SP.Gia - DELETED.GiaGiam) * DELETED.SoLuong) AS SUM_THANHTIEN_BOT
 		FROM INSERTED
-		INNER JOIN DELETED ON INSERTED.MaHD = DELETED.MaHD AND INSERTED.MaSP = DELETED.MaSP
-		INNER JOIN SanPham SP ON inserted.MaSP = SP.Ma
+			INNER JOIN DELETED ON INSERTED.MaHD = DELETED.MaHD AND INSERTED.MaSP = DELETED.MaSP
+			INNER JOIN SanPham SP ON inserted.MaSP = SP.Ma
 		GROUP BY INSERTED.MaHD
 	) SUM_UPDATED
 	WHERE HoaDon.Ma = SUM_UPDATED.MaHD;
 END
-GO
 
-
+------------------------- DELETE -------------------------
+----------------------------------------------------------
 GO
 CREATE TRIGGER TRG_HDCT_HoaDonChiTiet_InsteadOfDelete
 ON HoaDonChiTiet
@@ -104,14 +72,12 @@ BEGIN
 	FROM DELETED
 	WHERE HoaDonChiTiet.MaHD = DELETED.MaHD AND HoaDonChiTiet.MaSP = DELETED.MaSP
 
-		
 	UPDATE HoaDon
 	SET HoaDon.TongTien -= SUM_DELETED.SUM_THANHTIEN
 	FROM (
-		SELECT DELETED.MaHD, SUM(DELETED.ThanhTien) SUM_THANHTIEN
+		SELECT DELETED.MaHD, SUM(DELETED.ThanhTien) AS SUM_THANHTIEN
 		FROM DELETED
 		GROUP BY DELETED.MaHD
 	) SUM_DELETED
 	WHERE HoaDon.Ma = SUM_DELETED.MaHD;
 END
-GO
