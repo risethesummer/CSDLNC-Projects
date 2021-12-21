9b0d189b6b22e5fda53dc42bfee9ab2dc4b5bd5f
19127445 <64112482+19127445@users.noreply.github.com>
2021-12-21 12:56:46 +0700

    19127445
    store procedure tao don hang

---
+++ BT_VatLy/19127445_PROCEDURE_CreateOrder.sql
@@ -0,0 +1,29 @@
+﻿CREATE TYPE PRODUCT_QUANTITY AS TABLE
+(
+	ITEM_NUMBER CHAR(6),
+	QUANTITY INT
+);
+GO
+
+
+CREATE PROCEDURE CreateOrder @OrderNumber CHAR(8),@CustomerIdentifer CHAR(6),@OrderDate DATETIME,@ShippingStreetAddress nvarchar(30),@ShippingCity nvarchar(20),
+							 @ShippingState bit, @ShippingZipCode char(6), @CustomerCreditCardNumber char(16), @TotalPrice decimal(15, 2), @PRODUCT_QUANTITY PRODUCT_QUANTITY READONLY
+AS 
+BEGIN
+	--TINH TOTAL PRICE
+	SET @TotalPrice = (SELECT SUM(PQ.QUANTITY * AI.ItemPrice)
+						FROM Advertised_Item AI JOIN  @PRODUCT_QUANTITY PQ ON AI.ItemNumber = PQ.ITEM_NUMBER)
+	--TAO DON HANG
+	INSERT INTO Orders(OrderNumber,CustomerIdentifer,OrderDate,ShippingStreetAddress,ShippingCity,ShippingState,ShippingZipCode,CustomerCreditCardNumber,TotalPrice)
+	VALUES(@OrderNumber,@CustomerIdentifer,@OrderDate,@ShippingStreetAddress,@ShippingCity, @ShippingState,@ShippingZipCode,@CustomerCreditCardNumber,
+			@TotalPrice)
+
+	--TAO CHI TIET DON HANG
+	INSERT INTO Ordered_Item(ItemNumber,OrderNumber,QuantityOrdered,SellingPrice,TotalPriceOrderedItem)
+	SELECT PQ.ITEM_NUMBER,@OrderNumber,PQ.QUANTITY,AI.ItemPrice, PQ.QUANTITY*AI.ItemPrice
+	FROM @PRODUCT_QUANTITY PQ JOIN Advertised_Item AI ON PQ.ITEM_NUMBER = AI.ItemNumber
+
+	--UPDATE SO LAN DAT HANG CUA KHACH HANG
+	UPDATE Customer
+	SET CustomerTotalOrder = CustomerTotalOrder+1
+END
